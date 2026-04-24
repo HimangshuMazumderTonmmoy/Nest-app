7e1f8ab3675b8807732fb856e9e8020ce7ec6e73
Himangshu Mazumder <himangshualgo@gmail.com>
2026-04-24 23:34:02 +0600

    Implement CRUD service methods for course management

--- course-management-api/src/course/course.service.ts
+++ course-management-api/src/course/course.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class CourseService {}
+export class CourseService {
+    getAllCourses(): string {
+        return "Get all courses";
+    }
+
+    getCourseById(id: string): string {
+        return `Get Course with ID: ${id}`;
+    }
+
+    createCourse(): string {
+        return "Create Course";
+    }
+
+    updateCourse(id: string): string {
+        return `Update Course ${id}`;
+    }
+
+    patchCourse(id: string): string {
+        return `Patch Course ${id}`;
+    }
+
+    deleteCourse(id: string): string {
+        return `Delete Course ${id}`;
+    }
+}
