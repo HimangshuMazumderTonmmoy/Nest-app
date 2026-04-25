dcdc588fef4f21f91dab878cd748a59112ce020e
Himangshu Mazumder <himangshualgo@gmail.com>
2026-04-25 17:24:13 +0600

    Implement enrollment service methods with course dependency injection

--- university-system-api/src/enrollment/enrollment.service.ts
+++ university-system-api/src/enrollment/enrollment.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { CourseService } from 'src/course/course.service';
 
 @Injectable()
-export class EnrollmentService {}
+export class EnrollmentService {
+    constructor(private courseService: CourseService) {}
+
+    enrollStudent(studentName: string, courseId: number): {message: string, student: string, course: {message: string, ID: number}} {
+        const course: {message: string, ID: number} = this.courseService.getCourseById(courseId);
+        return {message: "Student enrolled successfully", student: studentName, course: course}
+    }
+
+    getEnrollments(): {message: string, data: {message: string, student: string, course: {message: string, ID: number}}[]} {
+        return { message: 'All enrollments fetched', data: [] }
+    }
+}
