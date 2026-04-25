489242dc9d1d9a37b86e8a2441f6989f23e8269d
Himangshu Mazumder <himangshualgo@gmail.com>
2026-04-25 16:08:19 +0600

    Implement course service methods for fetching and creating courses

---
+++ university-system-api/dist/course/course.service.d.ts
@@ -0,0 +1,17 @@
+export declare class CourseService {
+    getAllCourses(): {
+        message: string;
+        data: any[];
+    };
+    getCourseById(id: number): {
+        message: string;
+        ID: number;
+    };
+    createCourse(name: string, code: string): {
+        message: string;
+        data: {
+            name: string;
+            code: string;
+        };
+    };
+}
--- university-system-api/src/course/course.service.ts
+++ university-system-api/src/course/course.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class CourseService {}
+export class CourseService {
+    getAllCourses(): { message: string, data: any[] } {
+        return { message: "All courses fetched", data: [] }
+    }
+
+    getCourseById(id: number): { message: string, ID: number } {
+        return {message: "Course fetched", ID: id}
+    }
+
+    createCourse(name: string, code: string): {message: string, data: {name: string, code: string}} {
+        return {message: "Course created", data: {name, code}}
+    }
+}
