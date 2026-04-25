f32f01f0b164c0122cbcd5e560b0e8813cdc9915
Himangshu Mazumder <himangshualgo@gmail.com>
2026-04-25 15:24:10 +0600

    Implement enrollment module with controller, and service

--- university-system-api/src/app.module.ts
+++ university-system-api/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CourseModule } from './course/course.module';
+import { EnrollmentModule } from './enrollment/enrollment.module';
 
 @Module({
-  imports: [CourseModule],
+  imports: [CourseModule, EnrollmentModule],
   controllers: [AppController],
   providers: [AppService],
 })
