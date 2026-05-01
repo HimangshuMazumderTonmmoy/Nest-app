eee98da082329ead3c5a041cb30ce65ad80cc3b0
Himangshu Mazumder <himangshualgo@gmail.com>
2026-05-01 18:38:12 +0600

    Configure TypeORM PostgreSQL connection in App module

--- product-inventory-api/src/app.module.ts
+++ product-inventory-api/src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [
+    ProductsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'kraatoo',
+      password: 'Hindutava@5000',
+      database: 'product_inventory_db',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
