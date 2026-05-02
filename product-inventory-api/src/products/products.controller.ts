37c6707c07443dca206ef7b0499f3aa0c32ce0a5
Himangshu Mazumder <himangshualgo@gmail.com>
2026-05-02 19:32:12 +0600

    Implement product creation endpoint in ProductsController

--- product-inventory-api/src/products/products.controller.ts
+++ product-inventory-api/src/products/products.controller.ts
@@ -1,4 +1,14 @@
 import { Controller } from '@nestjs/common';
+import { ProductsService } from './products.service';
+import { CreateProductDto } from './dtos/create-product.dto';
+import { Put, Body, Post, Get, Param, Delete, Patch } from '@nestjs/common';
 
 @Controller('products')
-export class ProductsController {}
+export class ProductsController {
+    constructor(private readonly productsService: ProductsService) {}
+
+    @Post()
+    async create(@Body() dto: CreateProductDto) {
+        return this.productsService.create(dto);
+    }
+}
