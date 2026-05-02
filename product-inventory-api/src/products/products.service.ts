6167427fb33ec4d2110d29821d25a839d1e0f22f
Himangshu Mazumder <himangshualgo@gmail.com>
2026-05-02 19:00:07 +0600

    Implement create product service method with repository injection

--- product-inventory-api/src/products/products.service.ts
+++ product-inventory-api/src/products/products.service.ts
@@ -1,8 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Products } from './entities/products.entity';
 import { Repository } from 'typeorm';
 import { CreateProductDto } from './dtos/create-product.dto';
+import { PartialUpdateProductDto } from './dtos/partial-update-product.dto';
+import { UpdateProductDto } from './dtos/update-product.dto';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+    constructor (
+        @InjectRepository(Products)
+        private readonly productsRepo: Repository<Products>,
+    ) {}
+
+    async create (dto: CreateProductDto): Promise<{message: string, data: Products}> {
+        const productEntity = this.productsRepo.create(dto);
+        const savedProduct = await this.productsRepo.save(productEntity);
+
+        return {
+            message: "Product created successfully",
+            data: savedProduct,
+        };
+    }
+}
