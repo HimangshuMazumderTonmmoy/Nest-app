5ef796e68b06bbc8b3fd49a08f23f03319a683c8
Himangshu Mazumder <himangshualgo@gmail.com>
2026-05-02 15:52:04 +0600

    Inject Products entity into Products module and service using TypeORM

--- product-inventory-api/src/products/products.service.ts
+++ product-inventory-api/src/products/products.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Products } from './entities/products.entity';
+import { Repository } from 'typeorm';
+import { CreateProductDto } from './dtos/create-product.dto';
 
 @Injectable()
 export class ProductsService {}
