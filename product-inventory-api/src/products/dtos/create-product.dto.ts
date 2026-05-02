7ae269c8eedcf84df614c26d4c5a7c23b2f736e2
Himangshu Mazumder <himangshualgo@gmail.com>
2026-05-02 15:16:51 +0600

    Add validation decorators to CreateProductDto fields

--- product-inventory-api/src/products/dtos/create-product.dto.ts
+++ product-inventory-api/src/products/dtos/create-product.dto.ts
@@ -1,9 +1,28 @@
+import { IsBoolean, IsInt, IsNotEmpty, IsNumber, IsOptional, IsPositive, isPositive, IsString, Min } from "class-validator";
 
 export class CreateProductDto {
+    @IsString()
+    @IsNotEmpty()
     name: string;
+
+    @IsString()
+    @IsOptional()
     description: string;
+
+    @IsNumber()
+    @IsPositive()
     price: number;
+
+    @IsInt()
+    @IsOptional()
+    @Min(0)
     stock: number;
+
+    @IsString()
+    @IsNotEmpty()
     category: string;
+
+    @IsBoolean()
+    @IsOptional()
     isActive: boolean;
 }
